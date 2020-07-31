ae26b1f8465a6e38e4c6401e7ecd92cdc05f6fc6
emdoct <62918095+emdoct@users.noreply.github.com>
2020-07-31 16:45:38 +1000

    footer and form add

--- js/script.js
+++ js/script.js
@@ -2,10 +2,26 @@ $(function(){
 
 	$('.burger').on('click', function(){
 		$('.mobile-menu__list').toggleClass('menu__hide');
+		$('.burger').toggleClass('burger__hide');
 		
 		
  
 	});
+
+	$('.analysis__btn').on('click', function(event){
+		event.preventDefault();
+	});
+
+	$('.analysis__btn').on('click', function(){
+		$('.analysis-form__feeld').toggleClass('feeld__hide');
+	});
+
+	$('.home__scroll').on('click', function (event) {
+
+	    	   let home = $(this).attr('href');
+	    	   let top = $(home).offset().top;
+	    	  $('body,html').animate({scrollTop: top}, 1000);
+	    });
 
 
 
